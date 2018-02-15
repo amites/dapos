01aa864b93aca8af2caf29abd95a69b930c79dac
Christopher McGregor <chris@mcgregor.io>
2018-02-15 10:20:58 -0800

    Initial checkin

---
+++ core/dapos_service.go
@@ -0,0 +1,37 @@
+package core
+
+import (
+	"sync"
+	"net"
+)
+
+// DAPoSService
+type DAPoSService struct {
+	running bool
+}
+
+// NewDAPoSService
+func NewDAPoSService() *DAPoSService {
+	return &DAPoSService{
+		running: false,
+	}
+}
+
+// Name
+func (daposService *DAPoSService) Name() string {
+	return "DAPoSService"
+}
+
+// IsRunning
+func (daposService *DAPoSService) IsRunning() bool {
+	return daposService.running
+}
+
+// Register
+func (daposService *DAPoSService) Register(listener *net.Listener) {
+
+}
+
+// Go
+func (daposService *DAPoSService) Go(waitGroup *sync.WaitGroup) {
+}
